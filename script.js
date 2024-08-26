97c546639a95b8c392d433cde3523a0adcf6271a
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2024-08-26 23:50:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+        let angle = 0;
+
+        function rotateLine() {
+            angle += 2;
+
+           document.getElementById('line').style.transform = `rotate(${angle}deg)`;
+
+            requestAnimationFrame(rotateLine);
+        }
+
+        rotateLine();
